e3de3120bf4f26d7a5a31a5ed7fad5963a5e6432
Fan Young <ya1995fatw@gmail.com>
2019-11-07 12:33:35 -0800

    Update predictor.py

--- demo/predictor.py
+++ demo/predictor.py
@@ -63,7 +63,7 @@ class VisualizationDemo(object):
                 instances = predictions["instances"].to(self.cpu_device)
                 vis_output = visualizer.draw_instance_predictions(predictions=instances)
 
-        return predictions, vis_output
+        return predictions, vis_output, image.shape
 
     def _frame_from_video(self, video):
         while video.isOpened():
